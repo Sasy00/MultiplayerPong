307a5cc161910981b6af6888d27958cb318251a4
Sasy00 <super.salvatore00@gmail.com>
2022-07-15 18:33:06 +0000

    rewrited Player class as ES6 class

---
+++ pong/PlayerClass.js
@@ -0,0 +1,40 @@
+
+class Player {
+    constructor(id, x, y) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.input = 0;     //+1 goes down, -1 goes up, 0 is neutral
+        this.score = 0;
+    }
+    /**
+     * @param {number} x
+     */
+    set input(x) {
+        if (x > 0) {
+            this.input = 1;
+        }
+        else if (x < 0) {
+            this.input = -1;
+        }
+        else {
+            this.input = 0;
+        }
+    }
+
+    get position() { return { x: this.x, y: this.y }; }
+    set position(pos) { this.x = pos.x; this.y = pos.y; }
+
+    update() {
+        this.y += this.input;
+    }
+
+    incrementScore() {
+        this.score++;
+    }
+    resetScore() {
+        this.score = 0;
+    }
+}
+
+module.exports.Player = Player;
